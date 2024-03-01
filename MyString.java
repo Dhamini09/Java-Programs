70d9cf45ba71df73785253434753e28756672347
Dhamini Y <159795867+Dhamini09@users.noreply.github.com>
2024-03-01 17:30:43 +0530

    Add files via upload

---
+++ MyString.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class MyString{
+public static void main(String[]args){
+Scanner scan=new Scanner(System.in);
+int num=scan.nextInt();
+int num2=scan.nextInt();
+int res=num+num2;
+System.out.print(res);
+}
+}
+
